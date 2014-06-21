b6edcc4e3af8b829019b0f24d9737acd26105ab7
davidcox95 <davidcox95@comcast.net>
2014-06-20 21:07:08 -0700

    First commit of xcode files

---
+++ MachLearn/LinReg.h
@@ -0,0 +1,14 @@
+//
+//  LinReg.h
+//  MachLearn
+//
+//  Created by David Cox on 6/20/14.
+//  Copyright (c) 2014 David Cox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LinReg : NSObject
+
+
+@end
---
+++ MachLearn/LinReg.m
@@ -0,0 +1,13 @@
+//
+//  LinReg.m
+//  MachLearn
+//
+//  Created by David Cox on 6/20/14.
+//  Copyright (c) 2014 David Cox. All rights reserved.
+//
+
+#import "LinReg.h"
+
+@implementation LinReg
+
+@end
